258ff7c954b98129c6f0808a46b4d9e1e3db95db
floodinodisha <139390969+floodinodisha@users.noreply.github.com>
2024-07-30 20:22:51 +0530

    frontend

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat libero voluptates quibusdam temporibus voluptatibus ullam porro, facilis reiciendis aspernatur fuga assumenda et odio suscipit tenetur! Rem, deleniti accusantium iure reiciendis ipsam corporis ad itaque. Natus, expedita placeat aspernatur libero distinctio illum ea velit nihil, voluptatum, quidem incidunt nisi repellendus neque. amet consectetur adipisicing elit. Officia velit id, omnis animi expedita blanditiis a quas illum atque. Explicabo cumque ab odio exercitationem enim maiores cupiditate natus rerum deleniti.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi molestiae nobis temporibus veritatis provident impedit distinctio autem repellendus possimus facere eius voluptates rem veniam, assumenda vero! Optio praesentium laboriosam quae repudiandae eum pariatur harum architecto. Perferendis distinctio recusandae vero repellat.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
